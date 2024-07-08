b28b6516d12173c751c7763027e50e3d028046d0
HoKhanhDang <121717409+HoKhanhDang@users.noreply.github.com>
2024-07-08 11:43:34 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'https://ecom-app-server-taupe.vercel.app/api',
+    baseURL: 'https://ecom-app-server-rzrl.vercel.app/api',
 });
 // Add a request interceptor
 instance.interceptors.request.use(
